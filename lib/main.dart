7a954e4a98f0dc3632b9ba603ae4b520f5302f4c
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-14 22:07:02 -0700

    53-string interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
+const name = "Marianka";
+
 void main(){
   runApp (const Center(
-    child: Text('Hello world! My first apples!', 
+    child: Text('Hello $name! My first apples!',
     textDirection:TextDirection.ltr,
     style: TextStyle(
       fontWeight:FontWeight.bold,
       fontSize: 40,
-      color: Color.fromARGB(255, 224, 13, 52)
+      color: Colors.blueAccent,
     ) )
   ));
 }
